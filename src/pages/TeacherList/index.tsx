723e83d8b4f03b902a9b2e45863f7e76825b4c88
Mateus Soares da Rocha <rocha.mateus96@gmail.com>
2020-08-05 20:09:42 -0300

    Add simple teacher pages

---
+++ src/pages/TeacherList/index.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+import logoImg from "../../assets/images/logo.svg";
+import landingImg from "../../assets/images/landing.svg";
+
+import studyIcon from "../../assets/images/icons/study.svg";
+import giveClassesIcon from "../../assets/images/icons/give-classes.svg";
+import purpleHeartIcon from "../../assets/images/icons/purple-heart.svg";
+
+import './styles.css'
+
+function TeacherList() {
+  return (
+    <h1>TeacherList</h1>
+
+  );
+}
+
+export default TeacherList;
