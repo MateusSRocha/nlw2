b9120d30e902d81931521ae846f769c7301a7c4c
Mateus Soares da Rocha <rocha.mateus96@gmail.com>
2020-08-05 21:15:21 -0300

    Configurate basic pages

--- src/pages/TeacherList/index.tsx
+++ src/pages/TeacherList/index.tsx
@@ -1,19 +1,15 @@
 import React from "react";
 
-import logoImg from "../../assets/images/logo.svg";
-import landingImg from "../../assets/images/landing.svg";
-
-import studyIcon from "../../assets/images/icons/study.svg";
-import giveClassesIcon from "../../assets/images/icons/give-classes.svg";
-import purpleHeartIcon from "../../assets/images/icons/purple-heart.svg";
+import PageHeader from "../../components/PageHeader";
 
 import './styles.css'
 
 function TeacherList() {
   return (
-    <h1>TeacherList</h1>
-
-  );
+    <div id="page-teacher-list" className="container">
+      <PageHeader title="Estes são os proffys disponíveis" />
+    </div>  
+  ); 
 }
 
 export default TeacherList;
