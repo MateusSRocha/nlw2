d50c43877a2372025eeb22b1c93acfd83e6857dc
Mateus Soares da Rocha <rocha.mateus96@gmail.com>
2020-08-08 18:54:45 -0300

    Update teacherItem

--- src/components/TeacherItem/index.tsx
+++ src/components/TeacherItem/index.tsx
@@ -1,41 +1,60 @@
-import React from 'react';
+import React from "react";
 
 import whatsappIcon from "../../assets/images/icons/whatsapp.svg";
 
-import './styles.css'
+import "./styles.css";
+import { Interface } from "readline";
+import api from "../../services/api";
 
-function TeacherItem() {
-    return (
-        <article className="teacher-item">
-          <header>
-            <img
-              src="https://images.unsplash.com/photo-1556302132-40bb13638500?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=755&q=80"
-              alt="Professor"
-            />
-            <div>
-              <strong>Patric Evra Lamá</strong>
-              <span>Quimíca</span>
-            </div>
-          </header>
-          <p>
-            Entusiasta das melhores tecnologias de quimíca avançada.
-            <br />
-            <br />
-            Apaixonado por explodir coisas em laboratório e por mudar a vida das
-            pessoas através de experiências
-          </p>
-          <footer>
-            <p>
-              Preço/hora
-              <strong>80,00</strong>
-            </p>
-            <button type="button">
-              <img src={whatsappIcon} alt="whatsapp" />
-              Entrar em contato
-            </button>
-          </footer>
-        </article>
-    );
+export interface Teacher {
+  id: number;
+  avatar: string;
+  bio: string;
+  name: string;
+  cost: number;
+  subject: string;
+  whatsapp: string;
 }
+
+interface TeacherItemProps {
+  teacher: Teacher;
+}
+
+const TeacherItem: React.FC<TeacherItemProps> = ({ teacher }) => {
+  function createNewConnection() {
+    api.post("connections", {
+      user_id: teacher.id,
+    });
+  }
+
+  return (
+    <article className="teacher-item">
+      <header>
+        <img src={teacher.avatar} alt={teacher.name} />
+        <div>
+          <strong>{teacher.name}</strong>
+          <span>{teacher.subject}</span>
+        </div>
+      </header>
+
+      <p>{teacher.bio}</p>
+
+      <footer>
+        <p>
+          Preço/hora
+          <strong>{teacher.cost}</strong>
+        </p>
+        <a
+          target="_blank"
+          onClick={createNewConnection}
+          href={`https://wa.me/${teacher.whatsapp}`}
+        >
+          <img src={whatsappIcon} alt="whatsapp" />
+          Entrar em contato
+        </a>
+      </footer>
+    </article>
+  );
+};
 
 export default TeacherItem;
